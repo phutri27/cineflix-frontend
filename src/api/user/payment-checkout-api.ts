985b852b65cdd5fc132254d6509ec760e2a09cac
phutri27 <triphu27@gmail.com>
2026-04-29 13:37:24 +0700

    fix: add /api prefix to payment route

--- src/api/user/payment-checkout-api.ts
+++ src/api/user/payment-checkout-api.ts
@@ -8,21 +8,21 @@ interface VnPayCheckoutResponse {
 }
 
 export const getVnpayUrl = async (): Promise<{success: boolean}> => {
-    const response = await axiosClient.get("/payment/vnpay-return")
+    const response = await axiosClient.get("/api/payment/vnpay-return")
     return response.data
 }
 
 export const createCheckoutSession = async (datas: BookingData): Promise<{redirectUrl: string, bookingId: string}> => {
-    const response = await axiosClient.post("/payment/create-checkout-session", {datas})
+    const response = await axiosClient.post("/api/payment/create-checkout-session", {datas})
     return response.data
 }
 
 export const createVnpayCheckoutSession = async (datas: BookingData): Promise<VnPayCheckoutResponse> => {
-    const response = await axiosClient.post("/payment/vnpay-checkout", {datas})
+    const response = await axiosClient.post("/api/payment/vnpay-checkout", {datas})
     return response.data
 }
 
 export const cancelStripeCheckoutSesison = async (sessionId: string) => {
-    const response = await axiosClient.delete(`/payment/${sessionId}`)
+    const response = await axiosClient.delete(`/api/payment/${sessionId}`)
     return response.data
 }
